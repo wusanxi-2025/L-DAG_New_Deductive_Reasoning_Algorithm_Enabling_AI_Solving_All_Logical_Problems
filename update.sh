5cbf5af99cb91eada9389a325d73ee9daee77b0f
wusanxi-2025 <wusanxi@gmail.com>
2025-07-26 11:18:03 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,12 +4,27 @@
 cd ./L-DAG_New_Deductive_Reasoning_Algorithm_Enabling_AI_Solving_All_Logical_Problems
 
 # Add the specific files you want to update
-# L-DAG_Deductive_Reasoning_Algorithm.pdf 
-# README.md
-# all-cl4-1.py
-# dag_DFS.py
+# name="L-DAG_Deductive_Reasoning_Algorithm.pdf" 
+# name="README.md"
+# name="dag_DFS.py"
+# name="61-paper.txt"
+# name="update.sh"
+# name="git.cmd"
+# name=".gitconfig"
+# name="3-6v-10p-64r.txt"
+# name="3-6v-10p-64r.out.csv"
+# name="FFL-allowed.png"
+# name="cl4.py"
+# name="a-8v-15p-256r.txt"
+# name="a-8v-15p-256r.out.xlsx"
+# name="L-DAG_Deductive_Reasoning_Algorithm-2025-07-22.docx"
+# name="L-DAG_Deductive_Reasoning_Algorithm-2025-07-16.pdf"
 
-name="61-paper.txt"
+git ls-files
+
+#git rm "all-cl4.py"
+
+# name="tem"
 git add "$name"
 
 # Commit with a message (you can edit this)
@@ -17,3 +32,5 @@ git commit -m "Update $name"
 
 # Push to GitHub
 git push
+
+git ls-files
