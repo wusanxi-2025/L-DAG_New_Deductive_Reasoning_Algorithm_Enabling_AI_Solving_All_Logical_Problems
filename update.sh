6c58be3d6156ea4264356504b39d86bfc6ba92f4
wusanxi-2025 <wusanxi@gmail.com>
2025-08-18 12:59:23 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,17 +14,26 @@ cd ./L-DAG_New_Deductive_Reasoning_Algorithm_Enabling_AI_Solving_All_Logical_Pro
 # name="3-6v-10p-64r.txt"
 # name="3-6v-10p-64r.out.csv"
 # name="FFL-allowed.png"
-# name="cl4.py"
 # name="a-8v-15p-256r.txt"
 # name="a-8v-15p-256r.out.xlsx"
 # name="L-DAG_Deductive_Reasoning_Algorithm-2025-07-22.docx"
 # name="L-DAG_Deductive_Reasoning_Algorithm-2025-07-16.pdf"
+# name="test-a-8v-15p-256r.txt"
+# name="8v.docx"
+# name="cl4.py"
+# name="d-8v-15p-256r.txt"
+# name="d-8v-15p-256r.out.xlsx"
+# name="c-8v-15p-256r.txt"
+# name="c-8v-15p-256r.out.xlsx"
+# name="c-v8-DAG.docx"
+# name="./AI_test/Claude-4-2025-08-17-test-example-1.pdf"
+# name="./AI_test/Claude-4-2025-08-14-test-example-2.pdf"
 
 git ls-files
 
-#git rm "all-cl4.py"
+#git rm "tmp"
 
-# name="tem"
+name="tmp"
 git add "$name"
 
 # Commit with a message (you can edit this)
