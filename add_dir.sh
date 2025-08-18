99abd4654eda5ea5f7c819d0838587f95e6d21d0
wusanxi-2025 <wusanxi@gmail.com>
2025-08-18 13:00:43 -0600

    Update add_dir.sh

---
+++ add_dir.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Enter your project directory
+cd ./L-DAG_New_Deductive_Reasoning_Algorithm_Enabling_AI_Solving_All_Logical_Problems
+
+git add AI_test/*.*
+git commit -m "Add files in AI_test (excluding subdirectories)"
+git push origin main
